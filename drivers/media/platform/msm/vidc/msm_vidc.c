d1d08d4b50337c531a9ada60a0087941128a6a02
Amit Shekhar <ashekhar@codeaurora.org>
2018-08-31 15:04:54 -0700

    msm: vidc: Fix color format for HW HEIC encode
    Fix get/set format for HW HEIC encode. Color format for HW HEIC
    encode is V4L2_PIX_FMT_NV12_512.
    
    Change-Id: Icf7e43d002ef2120818d3249859ab6425a8cf092
    Signed-off-by: Amit Shekhar <ashekhar@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -275,6 +275,9 @@ int msm_vidc_g_fmt(void *instance, struct v4l2_format *f)
 	case V4L2_PIX_FMT_NV12:
 		color_format = COLOR_FMT_NV12;
 		break;
+	case V4L2_PIX_FMT_NV12_512:
+		color_format = COLOR_FMT_NV12_512;
+		break;
 	case V4L2_PIX_FMT_NV12_UBWC:
 		color_format = COLOR_FMT_NV12_UBWC;
 		break;
@@ -296,6 +299,8 @@ int msm_vidc_g_fmt(void *instance, struct v4l2_format *f)
 			inst->prop.width[port]);
 	f->fmt.pix_mp.plane_fmt[0].reserved[0] = VENUS_Y_SCANLINES(color_format,
 			inst->prop.height[port]);
+	f->fmt.pix_mp.plane_fmt[0].sizeimage = VENUS_BUFFER_SIZE(color_format,
+			inst->prop.width[port], inst->prop.height[port]);
 
 exit:
 	return rc;
